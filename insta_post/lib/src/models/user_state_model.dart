35ebda752db82fad0fa981ef864b7ebba3e3f238
Huy Nguyen <huynguyen2412@gmail.com>
2020-10-18 19:26:51 -0700

    update user state model

--- insta_post/lib/src/models/user_state_model.dart
+++ insta_post/lib/src/models/user_state_model.dart
@@ -4,6 +4,7 @@ import 'package:insta_post/src/models/user_info.dart';
 class UserStateModel extends ChangeNotifier{
   bool isSignIn = false;
   UserInfoPODO userInfo;
+  Map<String, UserInfoPODO> userEmails = {}; //store user's email to look up for their nickname
 
   void setUserInfo(UserInfoPODO userInfoModel) => userInfo = userInfoModel;
   UserInfoPODO getUserInfo() => userInfo;
@@ -12,4 +13,12 @@ class UserStateModel extends ChangeNotifier{
     isSignIn = !isSignIn;
     notifyListeners();
   }
+
+  Map<String, UserInfoPODO> getUserEmail() => this.userEmails;
+
+  bool signOut(){
+    isSignIn = false;
+    userInfo = null;
+  }
+
 }
